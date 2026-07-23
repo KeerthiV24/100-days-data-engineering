7067f4f30d8fc02edff9bed6f21201dbf078110f
Keerthi V <152879212+KeerthiV24@users.noreply.github.com>
2026-07-24 00:48:18 +0530

    Create SQL query for patient province names
    Add SQL query to show patients with full province names.

---
+++ SQL_Problems_Practice/07_show_patients_with_full_province_name.sql
@@ -0,0 +1,13 @@
+-- Problem 7: Show patient's full province name
+
+-- Question:
+-- Show first name, last name, and the full province name of each patient.
+-- Example: 'Ontario' instead of 'ON'
+
+SELECT
+    first_name,
+    last_name,
+    province_name
+FROM patients
+JOIN province_names
+ON patients.province_id = province_names.province_id;
